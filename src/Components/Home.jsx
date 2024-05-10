5c07d979561cdc1098aa4ba025f0326acfb8d21b
Nilesh Jadhav <jadhavnilesh289@gmail.com>
2024-05-10 17:53:16 +0530

    first commit

---
+++ src/Components/Home.jsx
@@ -0,0 +1,24 @@
+import TextForm from './TextForm';
+import React, {useState} from 'react';
+import Alert from './Alert';
+
+export default function Home(props) {
+
+const[alert, setAlert] = useState(null);
+
+const showAlert = (message) =>{
+    setAlert({
+      msg: message
+    })
+    setTimeout(() => {
+        setAlert(null);
+    }, 1500);
+}
+
+  return (
+    <div>
+      {alert && <Alert alert={alert} />}
+      <TextForm showAlert={showAlert} headings="Enter a Text to Analyze" />
+    </div>
+  )
+}
